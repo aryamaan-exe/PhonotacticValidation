efa182490c4f7a7f0055c64a4808995462a67136
aryamaan-exe <aryamaan.27@gmail.com>
2026-01-18 19:39:28 +0530

    more rules, manner and place enums

--- main.java
+++ main.java
@@ -1,36 +1,70 @@
 import java.util.*;
 
+public enum Place {
+    BILABIAL, LABIODENTAL, DENTAL, ALVEOLAR, POSTALVEOLAR, RETROFLEX, PALATAL, UVULAR, PHARYNGEAL, GLOTTAL
+}
+
+public enum Manner {
+    PLOSIVE, FRICATIVE, NASAL, LIQUID, GLIDE, VOWEL
+}
+
 public class Phoneme {
 	private final String symbol;
 	private final boolean isVowel;
 	private static final List<String> knownVowels = new ArrayList<>(List.of("a", "e", "i", "o", "u"));
-    private final int sonority;
+    private final Integer sonority;
+    private Map<Manner, int> sonorityMap;
 
-	public Phoneme(String _symbol, boolean _isVowel, int _sonority) {
-		symbol = _symbol;
-		isVowel = _isVowel;
-        sonority = _sonority;
-	}
+    private static void createSonorityMap() {
+        sonorityMap.put(Manner.PLOSIVE, 1);
+        sonorityMap.put(Manner.FRICATIVE, 2);
+        sonorityMap.put(Manner.NASAL, 3);
+        sonorityMap.put(Manner.LIQUID, 4);
+        sonorityMap.put(Manner.GLIDE, 5);
+        sonorityMap.put(Manner.VOWEL, 6);
+    }
 
-	public Phoneme(String _symbol, int _sonority) {
+	public Phoneme(String _symbol, Boolean _isVowel, Integer _sonority) {
+        createSonorityMap();
+
 		symbol = _symbol;
-		isVowel = checkVowel(_symbol);
-        sonority = _sonority;
+        if (_isVowel == null) {
+            isVowel = checkVowel(_symbol);
+        } else {
+            isVowel = _isVowel;
+        }
+
+        if (_sonority == null) {
+            sonority = checkSonority(_symbol);
+        } else {
+            sonority = _sonority;
+        }
 	}
 
 	public static boolean checkVowel(String symbol) {
         return knownVowels.contains(symbol);
     }
 
-    public static String getSymbol() {
+    public static Manner checkManner(String symbol) {
+        if (checkVowel) return Manner.VOWEL;
+
+        return null;
+        // TODO: fill this out
+    }
+
+    public static int checkSonority(String symbol) {
+        return sonorityMap.get(checkManner(symbol));
+    }
+
+    public String getSymbol() {
         return symbol;
     }
 
-    public static boolean isVowel() {
+    public boolean isVowel() {
         return isVowel;
     }
 
-    public static int getSonority() {
+    public int getSonority() {
         return sonority;
     }
 }
@@ -47,18 +81,59 @@ public class Syllable {
     }
 
     public static boolean validOnset() {
-        for (int i = 0; i < onset.size() - 1; i++) {
-            if (onset.get(i).getSonority() >= onset.get(i + 1).getSonority()) {
+        if (onset.size() > 3) return false;
+
+        if (!(onset.get(0).getSymbol().equals("s"))) {
+            for (int i = 0; i < onset.size() - 1; i++) {
+                if (onset.get(i).getSonority() >= onset.get(i + 1).getSonority()) {
+                    return false;
+                }
+
+                if (onset.get(i).getSymbol().equals(onset.get(i + 1).getSymbol())) {
+                    return false;
+                }
+            }
+        }
+
+        for (Phoneme p : onset) {
+            if (p.getSymbol().equals("ŋ")) return false;
+        }
+
+        return true;
+    }
+
+    public static boolean validNucleus() {
+        if (nucleus.size() == 0) return false;
+        
+        int i = 0;
+        for (Phoneme p : nucleus) {
+            if (p.isVowel()) i++;
+        }
+
+        return i == 1;
+    }
+
+    public static boolean validCoda() {
+        for (int i = 0; i < coda.size() - 1; i++) {
+            if (coda.get(i).getSonority() <= coda.get(i + 1).getSonority()) {
+                return false;
+            }
+
+            if (coda.get(i).getSymbol().equals(coda.get(i + 1).getSymbol())) {
                 return false;
             }
         }
 
         return true;
     }
+
+    public static boolean validSyllable() {
+        return validOnset() && validNucleus() && validCoda(); 
+    }
 }
 
 public class main {
 	public static void main(String[] args) {
-
+        
 	}
 }
